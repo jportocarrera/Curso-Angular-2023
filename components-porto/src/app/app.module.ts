258573e8df6456e774c6efd7d42cbf44d69723ca
jportocarrera <jose.portocarrera@gmail.com>
2023-01-19 14:48:10 -0500

    Clase numero 3

--- components-porto/src/app/app.module.ts
+++ components-porto/src/app/app.module.ts
@@ -2,20 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ListaComponent } from './components/lista/lista.component';
-import { LoginComponent } from './components/login/login.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
-import { StudentsComponent } from './components/students/students.component';
+import { SocialmediaComponent } from './components/socialmedia/socialmedia.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListaComponent,
-    LoginComponent,
     NavbarComponent,
     ToolbarComponent,
-    StudentsComponent
+    SocialmediaComponent
   ],
   imports: [
     BrowserModule
